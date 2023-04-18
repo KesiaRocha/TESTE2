32ae5d23b1e761256ecd4a1999c7317a91b05785
KesiaRocha <124710521+KesiaRocha@users.noreply.github.com>
2023-04-18 15:31:25 -0300

    24 é a 21

---
+++ q_02/src/br/edu/principal/Principal.java
@@ -0,0 +1,24 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+double n = 0;
+double e = 0;
+double i = 0;
+double j = 0;
+double FatNum = 0;
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Digite o número: ");
+
+
+
+
+	}
+
+}
